ec8b084f5948f854e9939bdb077fb5c202b75aae
sharmanjeurkar-dev <sharmanjeurkar@gmail.com>
2025-12-01 14:53:12 +0530

    Created a transaction database file

--- app/src/main/java/com/example/upi_expense_tracker/data/Transaction.java
+++ app/src/main/java/com/example/upi_expense_tracker/data/Transaction.java
@@ -1,6 +1,27 @@
 package com.example.upi_expense_tracker.data;
 
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "transactions")
 public class Transaction {
 
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private double amount;
+    private String merchantName;
+    private long timeStamp;
+    private boolean isDebit;
+
+    // private String source; after adding bhimpay functionality
+
+    //Constructor
+
+    public Transaction(double amount, String merchantName, long timeStamp, boolean isDebit) {
+        this.amount = amount;
+        this.merchantName = merchantName;
+        this.timeStamp = timeStamp;
+        this.isDebit = isDebit;
+    }
 }
