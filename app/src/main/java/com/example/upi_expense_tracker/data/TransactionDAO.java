ce628c5c2d4176de9811095ebbe51d1cb06f9fba
sharmanjeurkar-dev <sharmanjeurkar@gmail.com>
2025-12-01 16:25:43 +0530

    DAO interface ready with basic tasks like insertion, selection and total amount

--- app/src/main/java/com/example/upi_expense_tracker/data/TransactionDAO.java
+++ app/src/main/java/com/example/upi_expense_tracker/data/TransactionDAO.java
@@ -1,4 +1,24 @@
 package com.example.upi_expense_tracker.data;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface TransactionDAO {
+
+    //Insert the data coming from the Notification Listener Service
+    @Insert
+    void insertTransaction(Transaction transaction);
+
+    //Selecting all the data and making it visible
+    @Query("Select * from transactions order by id ASC")
+    List <Transaction> getAllTransactions();
+
+    //Calculate the total spent
+    @Query("Select SUM(amount) from transactions where isDebit=1 ")
+    double getTotalSpent();
+
 }
