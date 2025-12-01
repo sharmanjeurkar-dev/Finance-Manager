1637072d98179cfe752a1cb68409d648483991b2
sharmanjeurkar-dev <sharmanjeurkar@gmail.com>
2025-12-01 23:28:37 +0530

    Added functionality to the UI and used the TransactionDAO method for showing the saved value

--- app/src/main/java/com/example/upi_expense_tracker/activity/MainActivity.java
+++ app/src/main/java/com/example/upi_expense_tracker/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.upi_expense_tracker.activity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +10,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.upi_expense_tracker.R;
+import com.example.upi_expense_tracker.data.AppDatabase;
+import com.example.upi_expense_tracker.data.Transaction;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+       TextView tvTransaction = findViewById(R.id.tv_transactions);
+
+       //Fetch data on background
+        AppDatabase.databaseWriteExecutor.execute(()->{
+
+            List<Transaction> list = AppDatabase.getDatabase(getApplicationContext())
+                    .transactionDAO()
+                    .getAllTransactions();
+
+            StringBuilder display = new StringBuilder();
+            for(Transaction t: list) {
+                display.append("Rs").append(t.getAmount());
+                display.append("\n------------------------------------\n");
+            }
+            runOnUiThread(() -> {
+                if (list.isEmpty()) {
+                    tvTransaction.setText("No expenses found yet.");
+                } else {
+                    tvTransaction.setText(display.toString());
+                }
+            });
+
         });
+
+
     }
 }
